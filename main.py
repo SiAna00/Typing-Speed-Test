372d644b52385554cb70433d979fad722a2645a3
anasi <asimicev@gmail.com>
2024-02-17 13:59:38 +0100

    change text.txt to text.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import random
+from text import words
 
 # Create GUI with Tkinter
 window = Tk()
@@ -7,6 +9,9 @@ window.geometry("500x500")
 window.configure(bg="Purple")
 
 # Generate random words
+random_words = random.choice(words, k=50)
+words_label = Label(text=random_words)
+words_label.pack()
 
 # Count time (1 minute)
 
---
+++ text.py
@@ -0,0 +1,10 @@
+words = ["inspect", "return", "blow", "sentence", "rustic", "ski", "faint", "amazing", "breakable", "quill", "summon", "beseech", "fruit", "obeisant", 
+        "fall", "torpid", "check", "cheese", "ajar", "renew", "five", "mundane", "subscribe", "have", "impair", "wren", "argue", "save", "military", "polish", 
+        "icicle", "absorb", "dizzy", "wealthy", "giants", "legs", "dizzy", "astonishing", "nerve", "desire", "scissors", "guide", "enter", "calculate", "voice", 
+        "horses", "freeze", "sign", "pen", "thought", "line", "aboriginal", "garden", "savor", "paper", "upset", "lettuce", "space", "habitual", "pluck", "brawny", 
+        "six", "price", "water", "temporary", "scabble", "guitar", "wiry", "optimize", "clothe", "vague", "lean", "ruddy", "carpenter", "mute", "stress", "face", 
+        "bent", "retain", "metal", "tough", "rid", "abject", "angry", "compare", "wet", "nurse", "stop", "hapless", "sanction", "mice", "power", "sponge", "chip", 
+        "introduce", "rightful", "equable", "boats", "knowledgeable", "unusual", "skid", "guard", "juicy", "identify", "breath", "vein", "prepare", "pet", "servant", 
+        "opt", "knotty", "rhythm", "erase", "magical", "latch", "inculcate", "kindhearted", "forgetful", "alike", "cushion", "guarded", "propose", "balloon", "saturate",
+        "cake", "rotten", "calculating", "board", "chief", "watery", "sew", "glue", "low", "condition", "seemly", "habitual", "perfect", "conduct", "grieving", "current", 
+        "upset", "chief", "cluttered", "hospitable", "disobey", "linen", "tight", "arm", "anger", "wire"]
