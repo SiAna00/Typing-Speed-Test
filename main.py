329c4fd67d345ed1bac0984ba0e0279a7271b64f
anasi <asimicev@gmail.com>
2024-02-17 13:33:08 +0100

    add requirements.txt and text.txt

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# Create GUI with Tkinter
+window = Tk()
+window.title("Typing Speed Test")
+window.geometry("500x500")
+window.configure(bg="Purple")
+
+# Generate random words
+
+# Count time (1 minute)
+
+# Count how many words per minute
+
+window.mainloop()
